47f1997dd1b71878f8df1ff6f3317cae3e362bf8
choikanghee <rkdgml1106@gmail.com>
2021-04-26 13:20:42 +0900

    update 1330

---
+++ 1330.py
@@ -0,0 +1,22 @@
+a, b = map(int, input().split())
+
+if(a > b):
+    print('>')
+elif(a < b):
+    print('<')
+else:
+    print('==')
+
+'''
+while(True):
+    a, b = map(int, input().split())
+    if(a >= -10000 and b >= -10000 and a <= 10000 and b <= 10000):
+        break
+
+if(a > b):
+    print('>')
+elif(a < b):
+    print('<')
+else:
+    print('==')
+'''
